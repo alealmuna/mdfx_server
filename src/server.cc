18af099b5df242daad89f4ea02dc6e5091c5fcab
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-06-24 21:31:02 -0400

    Handling Interrupt Signals
    Now it's possible to shutdown cleanly when interrupted with Ctrl-C or
    another signal such as SIGTERM.

--- src/server.cc
+++ src/server.cc
@@ -1,12 +1,39 @@
+#include <signal.h>
 #include <iostream>
+
 #include "include/proto_handler.h"
 
 using std::cout;
 using std::endl;
 
+static int s_interrupted = 0;
+static void s_signal_handler(int /*signal_value*/) {
+    s_interrupted = 1;
+}
+
+static void s_catch_signals(void) {
+    struct sigaction action;
+    action.sa_handler = s_signal_handler;
+    action.sa_flags = 0;
+    sigemptyset(&action.sa_mask);
+    sigaction(SIGINT, &action, NULL);
+    sigaction(SIGTERM, &action, NULL);
+}
+
 int main() {
   ProtoHandler phandler;
-  cout << "Amucan Market Data FX" << endl;
-  phandler.readMessageFromFile();
+
+  s_catch_signals();
+
+  while (1) {
+    cout << "Amucan Market Data FX" << endl;
+    phandler.readMessageFromFile();
+
+    if (s_interrupted) {
+      cout << "W: interrupt received, killing server..." << endl;
+      break;
+    }
+  }
+  cout << "Cleaning..." << endl;
   return 0;
 }
