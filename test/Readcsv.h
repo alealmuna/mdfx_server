0120fefbd98f9fbd4e7af33de684972410e17ea0
Titool <titool@titool-desktop.(none)>
2012-06-25 23:39:20 -0400

    readcsv and readdir functions, unit test readcsv

---
+++ test/Readcsv.h
@@ -0,0 +1,11 @@
+#ifndef _READCSV_H_
+#define _READCSV_H_
+#include <vector>
+#include <string>
+
+using namespace std; 
+// Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+vector<string> Readcsv(char* file);
+int StrToInt(string const& s);
+
+#endif  // _READCSV_H_
