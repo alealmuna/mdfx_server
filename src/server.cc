7d8c2474c342aa1f6121e5fd254094ea5125781d
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-06-27 07:52:32 -0400

    Multithreaded service
    A pool of workers is now responsible of sending replies.

--- src/server.cc
+++ src/server.cc
@@ -1,3 +1,5 @@
+#include <pthread.h>
+#include <unistd.h>
 #include <signal.h>
 #include <zmq.hpp>
 #include <iostream>
@@ -10,30 +12,27 @@ using std::endl;
 
 static int s_interrupted = 0;
 static void s_signal_handler(int /*signal_value*/) {
-    s_interrupted = 1;
+  s_interrupted = 1;
 }
 
 static void s_catch_signals(void) {
-    struct sigaction action;
-    action.sa_handler = s_signal_handler;
-    action.sa_flags = 0;
-    sigemptyset(&action.sa_mask);
-    sigaction(SIGINT, &action, NULL);
-    sigaction(SIGTERM, &action, NULL);
+  struct sigaction action;
+  action.sa_handler = s_signal_handler;
+  action.sa_flags = 0;
+  sigemptyset(&action.sa_mask);
+  sigaction(SIGINT, &action, NULL);
+  sigaction(SIGTERM, &action, NULL);
 }
 
-int main() {
+void *worker_routine(void *arg) {
+  //  Prepare our context and socket
+  zmq::context_t *context = (zmq::context_t *) arg;
+  zmq::socket_t socket(*context, ZMQ_REP);
+  socket.connect("inproc://workers");
+
   ProtoHandler phandler;
 
-  s_catch_signals();
-
-  cout << "Amucan Market Data FX" << endl;
   phandler.readRequestFromFile("test_request");
-
-  //  Prepare our context and socket
-  zmq::context_t context(1);
-  zmq::socket_t socket(context, ZMQ_REP);
-  socket.bind("tcp://*:5555");
 
   while (!s_interrupted) {
     zmq::message_t request;
@@ -87,9 +86,37 @@ int main() {
       break;
     }
   }
+  return (NULL);
+}
 
-  cout << "Exiting..." << endl;
+int main() {
+  cout << "Amucan Market Data FX" << endl;
+  s_catch_signals();
+
+  // Prepare our context and sockets
+  zmq::context_t context(1);
+  zmq::socket_t clients(context, ZMQ_ROUTER);
+  clients.bind("tcp://*:5555");
+  zmq::socket_t workers(context, ZMQ_DEALER);
+  workers.bind("inproc://workers");
+
+  // Launch pool of worker threads
+  for (int thread_nbr = 0; thread_nbr != 2; thread_nbr++) {
+    pthread_t worker;
+    pthread_create(&worker, NULL, worker_routine,
+      (reinterpret_cast<void *>(&context)));
+  }
+  // Connect work threads to client threads via a queue
+  try {
+    zmq::device(ZMQ_QUEUE, clients, workers);
+  } catch(zmq::error_t&) {
+    int errsv = errno;
+    cout << zmq_strerror(errsv) << endl;
+  }
+
   google::protobuf::ShutdownProtobufLibrary();
+  cout << "Exiting..." << endl;
+  cout << "Workers shutting down..." << endl;
 
   return 0;
 }
