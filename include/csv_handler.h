f30f4f27fbe25756056bc61cc72553a8c80e536b
Tito <renato@amucan.com>
2012-06-28 13:41:59 -0400

    update csv_handler class.

--- include/csv_handler.h
+++ include/csv_handler.h
@@ -2,15 +2,17 @@
 #define INCLUDE_CSV_HANDLER_H_
 #include <string>
 #include <vector>
+#include "include/constants.h"
 
 using std::string;
 using std::vector;
 
 class CsvHandler {
   public:
-    vector < vector<string> > readcsv(vector<string> files);
-    vector<string> readdir();
-    vector < vector<string> > sortvec (vector < vector<string> > vec); 
+    vector <Quote> readcsv(vector<string> files);
+    int fixdate(string date, string hrs);
+    vector<string> readdir(char * dir);
+    void sortvec (vector <Quote> vec); 
 };
 
 #endif  // INCLUDE_CSV_HANDLER_H_
