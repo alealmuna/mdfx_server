c23373e72691d9944a061663591ee840b680f8fc
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-06-28 23:56:26 -0400

    Cpplint check on hdf5_handler

--- src/hdf5_handler.cc
+++ src/hdf5_handler.cc
@@ -1,11 +1,6 @@
-#ifdef OLD_HEADER_FILENAME
-#include <iostream.h>
-#else
-#include <iostream>
-#endif
 #include <string>
 #include <vector>
-#include <include/constants.h>
+#include <iostream>
 
 #ifndef H5_NO_NAMESPACE
 #ifndef H5_NO_STD
@@ -17,145 +12,134 @@
 
 #include "H5Cpp.h"
 #include "include/hdf5_handler.h"
+#include "include/constants.h"
 
 #ifndef H5_NO_NAMESPACE
     using namespace H5;
 #endif
 
-int writeToH5( vector <Quote> &quotes_v){
-  
-   try
-   {
-      int length = quotes_v.size();
-      Quote* quotes =  &quotes_v[0];
-      Exception::dontPrint();
-      /*
-       * Create the data space.
-       */
-      hsize_t dim[] = {length};   /* Dataspace dimensions */
-      DataSpace space( RANK, dim );
+int writeToH5(vector <Quote> &quotes_v) {
+  try {
+    int length = quotes_v.size();
+    Quote* quotes = &quotes_v[0];
+    Exception::dontPrint();
+    /*
+     * Create the data space.
+     */
+    hsize_t dim[] = {length};   /* Dataspace dimensions */
+    DataSpace space(RANK, dim);
 
-      /*
-       * Create the file.
-       */
-      H5File* file = new H5File( FILE_NAME, H5F_ACC_TRUNC );
+    /*
+     * Create the file.
+     */
+    H5File* file = new H5File(FILE_NAME, H5F_ACC_TRUNC);
 
-      /*
-       * Create the memory datatype.
-       */
-      CompType mtype1( sizeof(Quote) );
-      mtype1.insertMember( TSTAMP, HOFFSET(Quote, tstamp), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( NEMO, HOFFSET(Quote, nemo), PredType::NATIVE_INT);
-      mtype1.insertMember( BIDP, HOFFSET(Quote, bidp), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( BIDS, HOFFSET(Quote, bids), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( ASKP, HOFFSET(Quote, askp), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( ASKS, HOFFSET(Quote, asks), PredType::NATIVE_FLOAT);
+    /*
+     * Create the memory datatype.
+     */
+    CompType mtype1(sizeof(Quote));
+    mtype1.insertMember(TSTAMP, HOFFSET(Quote, tstamp), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(NEMO, HOFFSET(Quote, nemo), PredType::NATIVE_INT);
+    mtype1.insertMember(BIDP, HOFFSET(Quote, bidp), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(BIDS, HOFFSET(Quote, bids), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(ASKP, HOFFSET(Quote, askp), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(ASKS, HOFFSET(Quote, asks), PredType::NATIVE_FLOAT);
 
-      /*
-       * Create the dataset.
-       */
-      DataSet* dataset;
-      dataset = new DataSet(file->createDataSet(DATASET_NAME, mtype1, space));
+    /*
+     * Create the dataset.
+     */
+    DataSet* dataset;
+    dataset = new DataSet(file->createDataSet(DATASET_NAME, mtype1, space));
 
-      /*
-       * Write data to the dataset;
-       */
-      dataset->write( quotes, mtype1 );
+    /*
+     * Write data to the dataset;
+     */
+    dataset->write(quotes, mtype1);
 
-      /*
-       * Release resources
-       */
-      delete dataset;
-      delete file;
-   }  // end of try block
+    /*
+     * Release resources
+     */
+    delete dataset;
+    delete file;
+  }  // end of try block
 
-   // catch failure caused by the H5File operations
-   catch( FileIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  // catch failure caused by the H5File operations
+  catch(FileIException error) {
+    error.printError();
+    return -1;
+  }
 
-   // catch failure caused by the DataSet operations
-   catch( DataSetIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  // catch failure caused by the DataSet operations
+  catch(DataSetIException error) {
+    error.printError();
+    return -1;
+  }
 
-   // catch failure caused by the DataSpace operations
-   catch( DataSpaceIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  // catch failure caused by the DataSpace operations
+  catch(DataSpaceIException error) {
+    error.printError();
+    return -1;
+  }
 
-   // catch failure caused by the DataSpace operations
-   catch( DataTypeIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  // catch failure caused by the DataSpace operations
+  catch(DataTypeIException error) {
+    error.printError();
+    return -1;
+  }
 }
 
-int readFromH5( vector <Quote> &result ){
-   try
-   {
+int readFromH5(vector <Quote> &result) {
+  try {
+    Exception::dontPrint();
 
-      Exception::dontPrint();
+    H5File* file = new H5File(FILE_NAME, H5F_ACC_RDONLY);
 
-      H5File* file = new H5File( FILE_NAME, H5F_ACC_RDONLY );
+    CompType mtype1(sizeof(Quote));
+    mtype1.insertMember(TSTAMP, HOFFSET(Quote, tstamp), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(NEMO, HOFFSET(Quote, nemo), PredType::NATIVE_INT);
+    mtype1.insertMember(BIDP, HOFFSET(Quote, bidp), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(BIDS, HOFFSET(Quote, bids), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(ASKP, HOFFSET(Quote, askp), PredType::NATIVE_FLOAT);
+    mtype1.insertMember(ASKS, HOFFSET(Quote, asks), PredType::NATIVE_FLOAT);
 
-      CompType mtype1( sizeof(Quote) );
-      mtype1.insertMember( TSTAMP, HOFFSET(Quote, tstamp), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( NEMO, HOFFSET(Quote, nemo), PredType::NATIVE_INT);
-      mtype1.insertMember( BIDP, HOFFSET(Quote, bidp), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( BIDS, HOFFSET(Quote, bids), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( ASKP, HOFFSET(Quote, askp), PredType::NATIVE_FLOAT);
-      mtype1.insertMember( ASKS, HOFFSET(Quote, asks), PredType::NATIVE_FLOAT);
+    DataSet* dataset;
+    DataSpace* dataspace;
+    hsize_t dims_out[1];
 
-      DataSet* dataset;
-      DataSpace* dataspace;
-      hsize_t dims_out[1];
+    dataset = new DataSet(file->openDataSet(DATASET_NAME));
+    dataspace = new DataSpace(dataset->getSpace());
 
-      dataset = new DataSet (file->openDataSet( DATASET_NAME ));
-      dataspace = new DataSpace (dataset->getSpace());
+    int ndims = dataspace->getSimpleExtentDims(dims_out, NULL);
+    int data_size = dims_out[0];
 
-      int ndims = dataspace->getSimpleExtentDims( dims_out, NULL);
-      int data_size = dims_out[0];
+    Quote quotes[data_size];
+    dataset->read(quotes, mtype1);
 
-      Quote quotes[data_size];
-      dataset->read( quotes, mtype1 );
+    cout.precision(13);
+    for (int i = 0; i < data_size; i++) {
+       result.push_back(quotes[i]);
+    }
+    delete dataset;
+    delete file;
+  }
 
-      cout.precision(13);
-      for( int i = 0; i < data_size; i++){
-         result.push_back(quotes[i]);
-      }
-      delete dataset;
-      delete file;
-   }
+  catch(FileIException error) {
+    error.printError();
+    return -1;
+  }
 
-   catch( FileIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  catch(DataSetIException error) {
+    error.printError();
+    return -1;
+  }
 
-   catch( DataSetIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  catch(DataSpaceIException error) {
+    error.printError();
+    return -1;
+  }
 
-   catch( DataSpaceIException error )
-   {
-      error.printError();
-      return -1;
-   }
-
-   catch( DataTypeIException error )
-   {
-      error.printError();
-      return -1;
-   }
+  catch(DataTypeIException error) {
+    error.printError();
+    return -1;
+  }
 }
